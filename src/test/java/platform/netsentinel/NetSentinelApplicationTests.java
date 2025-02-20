c5a20feca4acd2d0f24ea0424d76c200ff68ad52
Viktor <viktormarimoric@gmail.com>
2025-02-20 18:51:40 +0200

    Initial commit for backend

---
+++ src/test/java/platform/netsentinel/NetSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package platform.netsentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetSentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
